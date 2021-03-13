a460c80f6d36001de7a76120c2038e06f8e520eb
th3james <th3james@katsuragi.local>
2021-03-13 13:58:54 +0000

    Pending test for BlogRenderer

--- blogbuilder/post_repository.py
+++ blogbuilder/post_repository.py
@@ -4,3 +4,6 @@ from pathlib import Path
 class PostRepository:
     def __init__(self, data_dir: Path) -> None:
         pass
+
+    def get_all(self) -> None:
+        pass
--- blogbuilder/tests/test_blog_renderer.py
+++ blogbuilder/tests/test_blog_renderer.py
@@ -2,10 +2,12 @@ from pathlib import Path
 from unittest import mock, TestCase
 
 from blogbuilder.blog_renderer import BlogRenderer
+from blogbuilder.post import Post
+from blogbuilder.post_repository import PostRepository
 
 
 class BlogRendererTest(TestCase):
-    def test_return_all_returns_index(self) -> None:
+    def test_renderer_all_returns_index(self) -> None:
         """
         it returns an enumerable containing index.html
         """
@@ -14,3 +16,17 @@ class BlogRendererTest(TestCase):
         result = blog_renderer.render_all()
 
         assert Path("index.html") in [p.path for p in result]
+
+    def test_renderer_returns_file_for_post(self) -> None:
+        """
+        given a PostRepository with a post
+        it returns a file with the post path
+        """
+        post = Post(Path("fancy-post.html"))
+        post_repository = mock.Mock(spec=PostRepository)
+        post_repository.get_all.return_value([post])
+
+        blog_renderer = BlogRenderer(post_repository, mock.Mock())
+        result = blog_renderer.render_all()
+
+        assert post.path in [p.path for p in result]
