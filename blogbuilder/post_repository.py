52102c6b0dba550b9ca564634d614e755cf51f28
th3james <th3james@katsuragi.local>
2021-03-14 14:30:01 +0000

    Make PostRepository return file path

--- blogbuilder/post_repository.py
+++ blogbuilder/post_repository.py
@@ -1,12 +1,22 @@
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, List
 
 from blogbuilder.post import Post
 
 
+@dataclass
 class PostRepository:
-    def __init__(self, data_dir: Path) -> None:
-        pass
+    data_dir: Path
 
     def get_all(self) -> Iterable[Post]:
-        return []
+        input_file_paths = [f for f in self.data_dir.glob("*") if f.is_file()]
+        output_file_paths: List[Path] = []
+        for input_file_path in input_file_paths:
+            chrooted_path = input_file_path.relative_to(self.data_dir)
+            parent_dir = chrooted_path.parents[0]
+            post_name = chrooted_path.stem
+            output_file_path = parent_dir / Path(f"{post_name}.html")
+            output_file_paths.append(output_file_path)
+
+        return [Post(p) for p in output_file_paths]
