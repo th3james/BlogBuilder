f4c311aa46dd0ced7d2c384bdd1e864ccbe99c74
th3james <th3james@katsuragi.local>
2021-03-20 14:45:14 +0000

    Port post file loader to TemporaryDirectory

--- blogbuilder/tests/test_post_file_loader.py
+++ blogbuilder/tests/test_post_file_loader.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from shutil import rmtree
+from tempfile import TemporaryDirectory
 from unittest import TestCase
 
 from blogbuilder.post_file_loader import PostFileLoader
@@ -11,10 +11,8 @@ class PostFileLoaderTest(TestCase):
         given a file
         it return a post with slug set based on file name
         """
-        input_dir = Path("blogbuilder/tests/post_file_loader_tests_data")
-
-        try:
-            input_dir.mkdir(parents=True)
+        with TemporaryDirectory() as td:
+            input_dir = Path(td)
 
             post_file_path = input_dir / "fancy-post.md"
             post_file_path.touch()
@@ -22,19 +20,15 @@ class PostFileLoaderTest(TestCase):
             result = PostFileLoader(input_dir).load(post_file_path)
 
             assert result.slug == "fancy-post"
-        finally:
-            rmtree(input_dir)
 
     def test_load_from_valid_file_laods_body(self) -> None:
         """
         given a file
         it return a post with the body read from the file
         """
-        input_dir = Path("blogbuilder/tests/post_file_loader_tests_data")
         body_text = "body text, yeah?"
-
-        try:
-            input_dir.mkdir(parents=True)
+        with TemporaryDirectory() as td:
+            input_dir = Path(td)
 
             post_file_path = input_dir / "fancy-post.md"
             post_file_path.write_text(body_text)
@@ -42,5 +36,3 @@ class PostFileLoaderTest(TestCase):
             result = PostFileLoader(input_dir).load(post_file_path)
 
             assert result.body == body_text
-        finally:
-            rmtree(input_dir)
