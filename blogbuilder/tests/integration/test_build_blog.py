48a3c776fce22294b995e705b4f37c26f8611247
th3james <th3james@katsuragi.local>
2021-03-21 12:52:40 +0000

    Add an integration test for template handling

--- blogbuilder/tests/integration/test_build_blog.py
+++ blogbuilder/tests/integration/test_build_blog.py
@@ -6,11 +6,11 @@ from blogbuilder.blog_builder import BlogBuilder
 
 
 class IntegrationBuildBlogTests(TestCase):
-    def test_build_example_blog(self) -> None:
+    def test_build_example_blog_post(self) -> None:
         """
         given the example blog directory
         and an output directory
-        it renders a page with the title
+        it renders a page with the post title
         """
         with TemporaryDirectory() as td:
             output_dir = Path(td)
@@ -22,3 +22,20 @@ class IntegrationBuildBlogTests(TestCase):
 
             with open(output_dir / "cool-post.html") as post:
                 assert "<h1>Nice post</h1>" in post.read()
+
+    def test_build_example_blog_with_template(self) -> None:
+        """
+        given the example blog directory
+        and an output directory
+        it renders a page with base template
+        """
+        with TemporaryDirectory() as td:
+            output_dir = Path(td)
+            BlogBuilder().build(
+                Path("blogbuilder/tests/example-test-app/posts"),
+                Path("blogbuilder/tests/example-test-app/templates"),
+                output_dir,
+            )
+
+            with open(output_dir / "cool-post.html") as post:
+                assert "<header>Dat blog title</header>" in post.read()
