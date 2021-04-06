64cc5e2203ce5e63fe6e6fa759f7bed39f5532a3
th3james <th3james@katsuragi.local>
2021-04-06 13:53:40 +0100

    Add basic arg parsing

--- src/blogbuilder/cli.py
+++ src/blogbuilder/cli.py
@@ -1,2 +1,9 @@
-def console_main() -> None:
-    print("Hello from Blog Builder")
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("name", help="Your name")
+args = parser.parse_args()
+
+
+def cli_main() -> None:
+    name = args.name
+    print(f"Hello {name} from Blog Builder")
