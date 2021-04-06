a6cc322b30572e00129ec98dc54cec96dbccb25f
th3james <th3james@katsuragi.local>
2021-04-06 14:02:13 +0100

    Make CLI work

--- src/blogbuilder/cli.py
+++ src/blogbuilder/cli.py
@@ -1,9 +1,17 @@
 import argparse
+from pathlib import Path
+
+from blogbuilder.blog_builder import BlogBuilder
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("name", help="Your name")
+parser.add_argument("post_dir", help="Path to post directory")
+parser.add_argument("templates_dir", help="Path to templates directory")
+parser.add_argument("output_dir", help="Path to ouput rendered blog")
 args = parser.parse_args()
 
 
 def cli_main() -> None:
-    name = args.name
-    print(f"Hello {name} from Blog Builder")
+    BlogBuilder().build(
+        Path(args.post_dir), Path(args.templates_dir), Path(args.output_dir)
+    )
