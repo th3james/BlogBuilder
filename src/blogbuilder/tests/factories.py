6ca58656f900c19085c0c0e26b1be5e9d1945d74
th3james <th3james@katsuragi.local>
2021-07-28 14:10:11 +0100

    Add blog factory

---
+++ src/blogbuilder/tests/factories.py
@@ -0,0 +1,13 @@
+from blogbuilder.post import Post
+
+
+def build_post(
+    slug: str = "post slug", title: str = "post title", body: str = "# hello\ncontent"
+) -> Post:
+    return Post(slug, title, body)
+
+
+def build_valid_post_content(title: str = "nice title", body: str = "Nice body") -> str:
+    return f"""title: {title}
+body:
+{body}"""
