de6c62a10ef9a54feff4a1fe259a47fa294272c7
th3james <th3james@katsuragi.local>
2021-09-20 21:54:44 +0100

    Add blog_name to the CLI

--- src/blogbuilder/cli.py
+++ src/blogbuilder/cli.py
@@ -5,6 +5,7 @@ from blogbuilder.blog_builder import BlogBuilder
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument("blog_name", help="The name of the blog")
 parser.add_argument("post_dir", help="Path to post directory")
 parser.add_argument("templates_dir", help="Path to templates directory")
 parser.add_argument("output_dir", help="Path to ouput rendered blog")
@@ -13,5 +14,8 @@ args = parser.parse_args()
 
 def cli_main() -> None:
     BlogBuilder().build(
-        Path(args.post_dir), Path(args.templates_dir), Path(args.output_dir)
+        args.blog_name,
+        Path(args.post_dir),
+        Path(args.templates_dir),
+        Path(args.output_dir),
     )
